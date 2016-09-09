85b11b313e9d18fa9cddab3b257a42c679a1877d
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2016-09-09 18:01:50 -0300

    :smile: adiciona teste instrumental

--- app/src/androidTest/java/com/example/mauricio/simpleDagger2WithMockito/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mauricio/simpleDagger2WithMockito/ExampleInstrumentedTest.java
@@ -2,12 +2,23 @@ package com.example.mauricio.simpleDagger2WithMockito;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +27,26 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.mauricio.simpledagger2", appContext.getPackageName());
+    }
+
+    @Test
+    public void changeTextInEditText2_mainActivity() throws Exception {
+        onView(withId(R.id.editText1))
+                .perform(typeText("teste"), closeSoftKeyboard());
+
+        onView(withId(R.id.editText2))
+                .check(matches(withText("teste")));
     }
 }
